5d67e6cdaff95f67b8f2b02c08978d4a92fd850d
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2020-05-23 12:20:22 +1000

    completed: secret handshake

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,4 +1,12 @@
 defmodule SecretHandshake do
+  @shake_moves [
+    "flip",
+    "jump",
+    "close your eyes",
+    "double blink",
+    "wink"
+  ]
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -14,6 +22,32 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
-  def commands(code) do
+  def commands(code) when is_integer(code) do
+    code
+    |> Integer.to_string(2)
+    |> String.pad_leading(5, "0")
+    |> String.split("")
+    |> Enum.drop(-1)
+    |> Enum.take(-5)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
+    |> Enum.map(&get_shake/1)
+    |> flip()
+    |> Enum.filter(fn x -> !is_nil(x) end)
+  end
+
+  defp get_shake({code, index}) do
+    if code == 1 do
+      Enum.at(@shake_moves, index)
+    end
+  end
+
+  defp flip(shake_list) do
+    if Enum.at(shake_list, 0) == "flip" do
+      {_, shake_list} = List.pop_at(shake_list, 0)
+      shake_list
+    else
+      Enum.reverse(shake_list)
+    end
   end
 end

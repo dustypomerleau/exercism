10e052716b43dae77ce10853418616518d46a182
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-20 20:25:26 +1100

    1 passed

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut s = String::new();
+    if n % 3 == 0 {
+        s.push_str("Pling")
+    } else if n % 5 == 0 {
+        s.push_str("Plang")
+    } else if n % 7 == 0 {
+        s.push_str("Plong")
+    } else {
+        s = n.to_string()
+    }
+    s
 }

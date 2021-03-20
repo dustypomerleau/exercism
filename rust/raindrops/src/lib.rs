18178739bfe828eca22ad3d83e69f742c1a47eef
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-20 20:29:26 +1100

    all tests passing for rust raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,12 +1,16 @@
 pub fn raindrops(n: u32) -> String {
     let mut s = String::new();
+
     if n % 3 == 0 {
         s.push_str("Pling")
-    } else if n % 5 == 0 {
+    }
+    if n % 5 == 0 {
         s.push_str("Plang")
-    } else if n % 7 == 0 {
+    }
+    if n % 7 == 0 {
         s.push_str("Plong")
-    } else {
+    }
+    if s == "" {
         s = n.to_string()
     }
     s

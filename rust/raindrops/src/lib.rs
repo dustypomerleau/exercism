0e369807ba9bbb2244a4b8ff3fb8cd0852a14e8e
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-20 21:18:38 +1100

    all passing with match

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,17 +1,14 @@
 pub fn raindrops(n: u32) -> String {
-    let mut s = String::new();
+    let s = match (n % 3, n % 5, n % 7) {
+        (0, 0, 0) => String::from("PlingPlangPlong"),
+        (_, 0, 0) => String::from("PlangPlong"),
+        (0, _, 0) => String::from("PlingPlong"),
+        (0, 0, _) => String::from("PlingPlang"),
+        (_, _, 0) => String::from("Plong"),
+        (0, _, _) => String::from("Pling"),
+        (_, 0, _) => String::from("Plang"),
+        (_, _, _) => n.to_string(),
+    };
 
-    if n % 3 == 0 {
-        s.push_str("Pling")
-    }
-    if n % 5 == 0 {
-        s.push_str("Plang")
-    }
-    if n % 7 == 0 {
-        s.push_str("Plong")
-    }
-    if s == "" {
-        s = n.to_string()
-    }
     s
 }

e07850d215ef08518c6c9672a5c46bc48474d5ba
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-20 19:53:31 +1100

    remove superfluous lifetime

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -30,7 +30,7 @@ impl Clock {
 }
 
 impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:0>2}:{:0>2}", self.time.hour(), self.time.minute())
     }
 }

d0abccf57d8475483d9b6436c324ce7b62097fdd
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-21 17:00:43 +1100

    anagram: passing all tests

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,16 +1,20 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut c: Vec<char> = word.to_lowercase().chars().into_iter().collect();
-    c.sort();
+    let c: Vec<char> = word.to_lowercase().chars().into_iter().collect();
+    let mut cs = c.clone();
+    cs.sort_unstable();
 
     let mut h = HashSet::new();
 
     for s in possible_anagrams {
         let s = *s;
         let mut pc: Vec<char> = s.to_lowercase().chars().into_iter().collect();
-        pc.sort();
         if pc == c {
+            break;
+        }
+        pc.sort_unstable();
+        if pc == cs {
             h.insert(s);
         }
     }

b7fdb0efed1c4814fac6aad515e67fe149456be0
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-21 12:29:47 +1100

    3 failing

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut c: Vec<char> = word.to_lowercase().chars().into_iter().collect();
+    c.sort();
+
+    let mut h = HashSet::new();
+
+    for s in possible_anagrams {
+        let s = *s;
+        let mut pc: Vec<char> = s.to_lowercase().chars().into_iter().collect();
+        pc.sort();
+        if pc == c {
+            h.insert(s);
+        }
+    }
+
+    h
 }

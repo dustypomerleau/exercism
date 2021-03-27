4c6a0791611bf14840fd10cc4f8450bde3b52b6d
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-27 11:35:05 +1100

    space age: planets implemented

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,6 +1,3 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
 pub struct Duration {
     years: f64,
@@ -31,13 +28,52 @@ pub struct Neptune;
 
 impl Planet for Mercury {
     fn years_during(d: &Duration) -> f64 {
-        d.years * 0.2408467
+        d.years / 0.2408467
     }
 }
-impl Planet for Venus {}
+
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 0.61519726
+    }
+}
+
 impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 1.8808158
+    }
+}
+
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 11.862615
+    }
+}
+
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 29.447498
+    }
+}
+
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 84.016846
+    }
+}
+
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        d.years / 164.79132
+    }
+}
+// Mercury: orbital period 0.2408467 Earth years
+// Venus: orbital period 0.61519726 Earth years
+// Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+// Mars: orbital period 1.8808158 Earth years
+// Jupiter: orbital period 11.862615 Earth years
+// Saturn: orbital period 29.447498 Earth years
+// Uranus: orbital period 84.016846 Earth years
+// Neptune: orbital period 164.79132 Earth years

7e8e9cb551ce674b89fada7c50095e1256d9597f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-30 22:31:10 +1100

    space age: switch to macro implementation

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -12,68 +12,26 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        d.years
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($planet:ident, $div:expr) => {
+        pub struct $planet;
 
-impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 0.2408467
-    }
+        impl Planet for $planet {
+            fn years_during(d: &Duration) -> f64 {
+                d.years / $div
+            }
+        }
+    };
 }
 
-impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 0.61519726
-    }
-}
-
-impl Planet for Earth {}
-
-impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 1.8808158
-    }
-}
-
-impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 11.862615
-    }
-}
-
-impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 29.447498
-    }
-}
-
-impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 84.016846
-    }
-}
-
-impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.years / 164.79132
-    }
-}
-// Mercury: orbital period 0.2408467 Earth years
-// Venus: orbital period 0.61519726 Earth years
-// Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
-// Mars: orbital period 1.8808158 Earth years
-// Jupiter: orbital period 11.862615 Earth years
-// Saturn: orbital period 29.447498 Earth years
-// Uranus: orbital period 84.016846 Earth years
-// Neptune: orbital period 164.79132 Earth years
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);

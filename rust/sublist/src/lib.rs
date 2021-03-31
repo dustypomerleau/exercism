94fb87e677f70e99de91ca0a73b129078cb2d139
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-03-31 22:37:47 +1100

    sublist: initial attempt, untested

---
+++ rust/sublist/src/lib.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    match a {
+        [] => Comparison::Sublist,
+        _ => match a == b {
+            true => Comparison::Equal,
+            _ => match a.len() < b.len() {
+                true => match is_sublist(a, b) {
+                    true => Comparison::Sublist,
+                    _ => Comparison::Unequal,
+                },
+                _ => match is_sublist(b, a) {
+                    true => Comparison::Superlist,
+                    _ => Comparison::Unequal,
+                },
+            },
+        },
+    }
+}
+
+fn is_sublist<T: PartialEq>(x: &[T], y: &[T]) -> bool {
+    let mut sub = false;
+    for i in 0..x.len() + 1 {
+        if x == &y[i..i + x.len()] {
+            sub = true;
+            break;
+        }
+    }
+    sub
+}

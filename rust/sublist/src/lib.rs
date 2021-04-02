f47a3ef322c0494f04a8904b5b440057befde887
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-04-02 19:43:20 +1100

    sublist: remove superfluous function

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -8,25 +8,6 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist2<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
-    match a == b {
-        true => Comparison::Equal,
-        _ => match a {
-            [] => Comparison::Sublist,
-            _ => match a.len() < b.len() {
-                true => match is_sublist(a, b) {
-                    true => Comparison::Sublist,
-                    _ => Comparison::Unequal,
-                },
-                _ => match is_sublist(b, a) {
-                    true => Comparison::Superlist,
-                    _ => Comparison::Unequal,
-                },
-            },
-        },
-    }
-}
-
 pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
     match a == b {
         true => Comparison::Equal,
